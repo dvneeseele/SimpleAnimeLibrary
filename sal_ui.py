081c664699281d92fba7e80647723778eeb7b9e8
dvneeseele <dvneeseele@gmail.com>
2021-03-12 13:03:40 -0600

    - menubar options/actions added for file and edit.

--- sal_ui.py
+++ sal_ui.py
@@ -4,3 +4,39 @@ import sys
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (QMainWindow, QToolBar, QSplitter, QHBoxLayout, QWidget, QAction, QStackedWidget, QListWidget, QTableWidget)
+
+
+class salUI():
+    def __init__(self):
+        super(salUI).__init__()
+
+
+    def setupUI(self, MainWindow):
+
+        # setup menubar
+
+        menubar = MainWindow.menubar()
+
+        self.mb_file = menubar.addMenu("File")
+        self.mb_edit = menubar.addMenu("Edit")
+        self.mb_view = menubar.addMenu("View")
+        self.mb_help = menubar.addMenu("Help")
+
+
+        # menubar file menu
+
+        self.mb_newAction = QAction("New Entry", MainWindow)
+        self.mb_file.addAction(self.mb_newAction)
+
+        # menubar edit menu
+
+        self.mb_editEntryAction = QAction("Edit Entry", MainWindow)
+        self.mb_edit.addAction(self.mb_editEntryAction)
+
+        # menubar view menu
+
+        # TODO
+
+        # menubar help menu
+
+        # TODO
