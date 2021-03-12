c731de63a9f6adbd65e2c1187a67f24dcc326700
dvneeseele <dvneeseele@gmail.com>
2021-03-12 14:27:56 -0600

    - created sal.py backend file. - added basic application imports

---
+++ sal.py
@@ -0,0 +1,32 @@
+#############################################################################
+# me
+#############################################################################
+
+
+from sal_ui import salUI
+import os
+import sys
+import sqlite3
+import requests
+
+from PyQt5 import Qt
+from PyQt5.QtGui import QIcon, QPixmap, QImage, QTextListFormat, QFont, QColor
+from PyQt5.QtCore import QEvent, Qt, QSize, QDate, QTime
+from PyQt5.QtWidgets import (QApplication, QMainWindow, QFormLayout, QLineEdit, QTabWidget, QWidget, QPushButton, QListWidgetItem, QLabel, QVBoxLayout, QStackedWidget,
+                            QColorDiaglog, QMessageBox, QFileDialog, QDialog, QFontDialog)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
