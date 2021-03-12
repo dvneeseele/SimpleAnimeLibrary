3d27a69bb40d6d5852c148320dc846db9f767839
dvneeseele <dvneeseele>
2021-03-12 14:50:37 -0600

    - fixed import issues among front and backend - added static list items to sidebar

--- sal.py
+++ sal.py
@@ -9,12 +9,39 @@ import sys
 import sqlite3
 import requests
 
+
 from PyQt5 import Qt
 from PyQt5.QtGui import QIcon, QPixmap, QImage, QTextListFormat, QFont, QColor
 from PyQt5.QtCore import QEvent, Qt, QSize, QDate, QTime
 from PyQt5.QtWidgets import (QApplication, QMainWindow, QFormLayout, QLineEdit, QTabWidget, QWidget, QPushButton, QListWidgetItem, QLabel, QVBoxLayout, QStackedWidget,
-                            QColorDiaglog, QMessageBox, QFileDialog, QDialog, QFontDialog)
+                            QColorDialog, QMessageBox, QFileDialog, QDialog, QFontDialog)
 
+
+
+class SAL_app(salUI):
+    def __init__(self):
+        super().__init__()
+
+        self.mainWindow = QMainWindow()
+        self.setupUI(self.mainWindow)
+
+        # Menubar functions
+
+        # TODO
+
+        # Toolbar functions
+
+        # TODO
+
+        # load function here
+
+
+
+
+
+
+    def getCurrentStack(self):
+        pass
 
 
 
