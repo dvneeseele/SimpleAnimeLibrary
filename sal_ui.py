544b23579c483a3e1b45b57538a8e61a13bd5437
dvneeseele <dvneeseele@gmail.com>
2021-03-12 12:52:32 -0600

    - mainwindow ui file created with basic imports

---
+++ sal_ui.py
@@ -0,0 +1,6 @@
+import os
+import sys
+
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (QMainWindow, QToolBar, QSplitter, QHBoxLayout, QWidget, QAction, QStackedWidget, QListWidget, QTableWidget)
