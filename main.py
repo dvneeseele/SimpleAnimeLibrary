1cde2009106a09ba25b9890143609f83022b46e9
dvneeseele <dvneeseele@gmail.com>
2021-03-22 18:49:12 -0500

    - Basic Jikan API support added - Grabs series title art and inserts into db as BLOB - Inserts into QLabel as QPixmap

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from PyQt5.QtWidgets import QApplication
 from sal import SAL_app
 
 application = QApplication(sys.argv)
-#application.setStyleSheet(open("qss/app.qss", "r")).read()
+#application.setStyleSheet(open("qss/app.qss", "r").read())
 application.setStyle('fusion')
 SAL = SAL_app()
 sys.exit(application.exec_())
