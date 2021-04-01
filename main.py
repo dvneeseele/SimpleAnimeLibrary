a6e0b115f615e519f46845e2b2fdcbd3d3048ee0
dvneeseele <dvneeseele>
2021-04-01 18:59:32 -0500

    - added 3 themes from qssstock - edited the synet.qss for QMenu color - added rough icons for add, delete, and edit - reordered buttons on toolbar - set table font and size

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ from PyQt5.QtWidgets import QApplication
 from sal import SAL_app
 
 application = QApplication(sys.argv)
-#application.setStyleSheet(open("qss/app.qss", "r").read())
+application.setStyleSheet(open("qss/SyNet/SyNet.qss", "r").read())
+# application.setStyleSheet(open("qss/Obit/Obit.qss", "r").read())
+# application.setStyleSheet(open("qss/DeepBox/DeepBox.qss", "r").read())
+# application.setStyleSheet(open("qss/Combinear/Combinear.qss", "r").read())
 application.setStyle('fusion')
 SAL = SAL_app()
 sys.exit(application.exec_())
